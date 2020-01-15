0805f3c8ba4d7cabb605d9587c0a80dc5a624837
Your Name <you@example.com>
2020-01-15 22:14:21 +0100

    Lazy Loaded implemented

--- dist/assets/js/lazyLoad.js
+++ dist/assets/js/lazyLoad.js
@@ -4,11 +4,12 @@ const lazyLoad = target => {
   const io = new IntersectionObserver((entires, observer) => {
     entires.forEach(entry => {
       if (entry.isIntersecting) {
+        console.log("LazyLoaded!");
+
         const img = entry.target;
         const src = img.getAttribute("data-lazy");
 
         img.setAttribute("src", src);
-
         observer.disconnect();
       }
     });
--- gulpfile.js
+++ gulpfile.js
@@ -15,68 +15,68 @@ sass.compiler = require("node-sass");
 // HTML Files Rename - Pretty-Urls
 
 const html = done => {
-    gulp
-        .src("app/html/templates/*.ejs")
-        .pipe(ejs().on("error", err => console.log(err)))
-        .pipe(
-            rename(path => {
-                if (path.basename != "index") {
-                    path.dirname = path.basename;
-                    path.basename = "index";
-                }
-                path.extname = ".html";
-            })
-        )
-        .pipe(gulp.dest("dist"))
-        .pipe(connect.reload());
-    done();
+  gulp
+    .src("app/html/templates/*.ejs")
+    .pipe(ejs().on("error", err => console.log(err)))
+    .pipe(
+      rename(path => {
+        if (path.basename != "index") {
+          path.dirname = path.basename;
+          path.basename = "index";
+        }
+        path.extname = ".html";
+      })
+    )
+    .pipe(gulp.dest("dist"))
+    .pipe(connect.reload());
+  done();
 };
 
 // SCSS Files Compiler
 
 const scss = done => {
-    gulp
-        .src("app/css/**/*.scss")
-        .pipe(sass().on("error", err => console.log(err)))
-        .pipe(gulp.dest("dist/assets/css"))
-        .pipe(connect.reload());
-    done();
+  gulp
+    .src("app/css/**/*.scss")
+    .pipe(sass().on("error", err => console.log(err)))
+    .pipe(gulp.dest("dist/assets/css"))
+    .pipe(connect.reload());
+  done();
 };
 
 // Javascript Compiler
 
 const javascript = done => {
-    gulp
-        .src("app/js/**/*.js")
-        .pipe(
-            babel({
-                presets: ["@babel/env"]
-            })
-        )
-        .pipe(gulp.dest("dist/assets/js"))
-        .pipe(connect.reload());
-    done();
+  gulp
+    .src("app/js/**/*.js")
+    // .pipe(
+    //     babel({
+    //         presets: ["@babel/env"]
+    //     })
+    // )
+    .pipe(gulp.dest("dist/assets/js"))
+    .pipe(connect.reload());
+  done();
 };
 
 // Json Compiler
 
 const json = done => {
-    gulp
-        .src("./app/json/*.json")
-        .pipe(gulp.dest("/dist/data"))
-        .pipe(connect.reload());
-    done();
+  gulp
+    .src("./app/json/*.json")
+    .pipe(gulp.dest("/dist/data"))
+    .pipe(connect.reload());
+  done();
 };
 
 // Image Compiler
 
 const images = done => {
-    gulp
-        .src("./app/images/*")
-        .pipe(imagemin())
-        .pipe(gulp.dest("dist/assets/images"))
-        .pipe(connect.reload());
-    done();
+  gulp
+    .src("./app/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/assets/images"))
+    .pipe(connect.reload());
+  done();
 };
 
 // --------------WATCHERS-----------
@@ -84,80 +84,85 @@ const images = done => {
 // Watch HTML
 
 const watchHtml = () => {
-    gulp.watch(
-        "./app/html/**/*.ejs", {
-            ignoreInitial: false
-        },
-        html
-    );
+  gulp.watch(
+    "./app/html/**/*.ejs",
+    {
+      ignoreInitial: false
+    },
+    html
+  );
 };
 
 // Watch SCSS
 
 const watchScss = () => {
-    gulp.watch(
-        "./app/css/**/*.scss", {
-            ignoreInitial: false
-        },
-        scss
-    );
+  gulp.watch(
+    "./app/css/**/*.scss",
+    {
+      ignoreInitial: false
+    },
+    scss
+  );
 };
 
 // Watch SCSS
 
 const watchJavascript = () => {
-    gulp.watch(
-        "./app/js/**/*.js", {
-            ignoreInitial: false
-        },
-        javascript
-    );
+  gulp.watch(
+    "./app/js/**/*.js",
+    {
+      ignoreInitial: false
+    },
+    javascript
+  );
 };
 
 // Watch Json
 
 const watchJson = () => {
-    gulp.watch(
-        "./app/json/*.json", {
-            ignoreInitial: false
-        },
-        json
-    );
+  gulp.watch(
+    "./app/json/*.json",
+    {
+      ignoreInitial: false
+    },
+    json
+  );
 };
 
 // Watch Json
 
 const watchImages = () => {
-    gulp.watch(
-        "./app/images/**/**", {
-            ignoreInitial: false
-        },
-        images
-    );
+  gulp.watch(
+    "./app/images/**/**",
+    {
+      ignoreInitial: false
+    },
+    images
+  );
 };
 
 // develop task
 
 gulp.task("dev", done => {
-    watchHtml();
-    watchScss();
-    watchJavascript();
-    watchJson();
-    watchImages();
-    connect.server({
-        livereload: true,
-        root: "dist"
-    });
-    done();
+  watchHtml();
+  watchScss();
+  watchJavascript();
+  watchJson();
+  watchImages();
+  connect.server({
+    livereload: true,
+    root: "dist"
+  });
+  done();
 });
 
 // Build
 
 gulp.task("build", done => {
-    html(done);
-    scss(done);
-    javascript(done);
-    json(done);
-    images(done);
-    done();
+  html(done);
+  scss(done);
+  javascript(done);
+  json(done);
+  images(done);
+  done();
 });
