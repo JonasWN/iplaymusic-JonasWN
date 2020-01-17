6cebb685db08e2ddb83c40a84435b9ed0332f0e4
Jonas Wessman Nielsen <5615825@RTS-365.dk>
2020-01-17 09:30:54 +0100

    playing site slider added

--- app/js/themeMode.js
+++ app/js/themeMode.js
@@ -17,18 +17,19 @@ const switchTheme = e => {
     }
 }
 
-toggle.addEventListener("click", switchTheme, false);
+if (toggle) {
+    toggle.addEventListener("click", switchTheme, false);
 
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'light') {
-        toggle.checked = true;
+        if (currentTheme === 'light') {
+            toggle.checked = true;
+        }
     }
 }
-
 
 
 // icons current color 
